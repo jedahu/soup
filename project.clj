f72acfc81f7497e9bd7d2bd6ed4a1fae2d3b0fed
Jeremy Hughes <jedahu@gmail.com>
2011-10-20 12:45:15 +1300

    soup: added rmerge-with and rmerge

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject
-  me.panzoo/soup "0.0.2"
+  me.panzoo/soup "0.0.3-SNAPSHOT"
 
   :description "A soup of clojurescript utilites.")
---
+++ src/me/panzoo/soup.cljs
@@ -0,0 +1,28 @@
+(ns me.panzoo.soup)
+
+(defn rmerge-with
+  "Merge maps recursively. Apply `f` to a level when it contains a non-map.
+
+  `f` should take a single argument which is a list of objects which may be maps."
+  [f & maps]
+  (apply
+    (fn m [& maps]
+      (if (every? #(or (map? %) (not %)) maps)
+        (apply merge-with m maps)
+        (f maps)))
+    maps))
+
+(defn rmerge
+  "Merge maps recursively. If a level contains a non-map, the value for that
+  level will be the result of merging the maps at the end of the sequence or it
+  will be the last item in the sequence."
+  [& maps]
+  (letfn [(f [& maps]
+            (let [maps* (reverse
+                          (take-while
+                            (or (map? x) (not x))
+                            (reversee maps)))]
+              (if (seq maps*)
+                (apply rmerge-with f ms*)
+                (last maps))))]
+    (apply rmerge-with f maps)))
