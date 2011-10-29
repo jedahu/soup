7418797329710f08a95222bd17c3bcffb5e57325
Jeremy Hughes <jedahu@gmail.com>
2011-10-29 14:08:09 +1300

    css: docstrings

--- src/me/panzoo/soup/css.cljs
+++ src/me/panzoo/soup/css.cljs
@@ -2,13 +2,17 @@
   (:use
     [clojure.string :only (join)]))
 
-(defn- wrap [n s]
+(defn- wrap
+  "Wrap `s` in `n` vectors."
+  [n s]
   (loop [n n acc s]
     (if (= 0 n)
       acc
       (recur (dec n) [acc]))))
 
 (defn- normalize-selector
+  "Normalize selector to the form `[[a b] [c d]]`, which represents the CSS
+  selector: 'a b, c d'."
   ([s n]
   (if (vector? s)
     (map #(normalize-selector % (dec n))  s)
@@ -16,11 +20,33 @@
   ([s]
    (normalize-selector s 2)))
 
-(defn rules [& rs]
+(defn rules
+  "Convert the vectors `rs` to CSS rules. See `css` for details."
+  [& rs]
   (join ";" (for [[k v] (partition 2 rs)]
               (str (name k) ":" v))))
 
 (defn css
+  "Convert the input to CSS. Input is one or more of the following vectors,
+  which represent a single selector-rules pairing:
+
+  `[:selector
+    :border \"1px solid red\"
+    :border-top \"green\"]`
+
+  Rule order is preserved so later rules can override earlier ones.
+ 
+  Property names are passed to `name` and values to `str`.
+
+  Selectors are normalized to the form `[[a b] [c d]]`, which represents the
+  CSS selector, 'a b, c d'. For examnple,
+
+  :.entity => [[\".entity\"]] => \".entity\"
+
+  [:.entity :.name] => [[\".entity\"] [\".name\"]] => \".entity, .name\"
+
+  [:.entity [:.name :> :.nick]] => [[\".entity\"] [\".name\" \">\" \".nick\"]]
+      => \".entity, .name > .nick\"."
   ([[selector & rule-list]]
    (str
      (join "," (map #(join " " %)
