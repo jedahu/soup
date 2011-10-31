d586f0ae99890ebebb79bc32e4f090ac3122bd86
Jeremy Hughes <jedahu@gmail.com>
2011-10-31 21:28:14 +1300

    test: require phantom

--- cljs/test/me/panzoo/soup/test.cljs
+++ cljs/test/me/panzoo/soup/test.cljs
@@ -1,22 +1,22 @@
 (ns me.panzoo.soup.test
   (:require
     [me.panzoo.soup :as s])
-  (:use-macros
-    [me.panzoo.soup.macros.phantom :only (assert)]))
+  (:require-macros
+    [me.panzoo.soup.macros.phantom :as p]))
 
-(assert
+(p/assert
   (= {:a 2} (s/rmerge {:a 1} {:a 2})))
 
-(assert
+(p/assert
   (= {:a 2 :c 3} (s/rmerge {:a {:b 1} :c 3} {:a 2})))
 
-(assert
+(p/assert
   (= {:a {:b 2} :c 3} (s/rmerge {:a {:b 1} :c 3} {:a {:b 2}})))
 
-(assert
+(p/assert
   (= [3] (s/rmerge {:a 1} {:a 2} [1] [3])))
 
-(assert
+(p/assert
   (= {:x 0} (s/rmerge {:a 1} {:b 2} "x" {:x 1} {:x 0})))
 
 (.exit js/phantom 0)
