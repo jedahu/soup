b36c0a618efb7ef2dce51166a1ea2c570a9be09b
Jeremy Hughes <jedahu@gmail.com>
2011-10-31 21:57:43 +1300

    rename phantom.assert back to passert

--- cljs/test/me/panzoo/soup/test.cljs
+++ cljs/test/me/panzoo/soup/test.cljs
@@ -1,20 +1,20 @@
 (ns me.panzoo.soup.test
   (:require
     [me.panzoo.soup :as s])
-  (:require-macros
-    [me.panzoo.soup.macros.phantom :as p]))
+  (:use-macros
+    [me.panzoo.soup.macros.phantom :only (passert)]))
 
-(p/assert
+(passert
   (= {:a 2} (s/rmerge {:a 1} {:a 2})))
 
-(p/assert
+(passert
   (= {:a 2 :c 3} (s/rmerge {:a {:b 1} :c 3} {:a 2})))
 
-(p/assert
+(passert
   (= {:a {:b 2} :c 3} (s/rmerge {:a {:b 1} :c 3} {:a {:b 2}})))
 
-(p/assert
+(passert
   (= [3] (s/rmerge {:a 1} {:a 2} [1] [3])))
 
-(p/assert
+(passert
   (= {:x 0} (s/rmerge {:a 1} {:b 2} "x" {:x 1} {:x 0})))
